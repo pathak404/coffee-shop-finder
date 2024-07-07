a3b6ee6d0ed31e4077802697d58c88ec82b2c691
Abhishek Kumar Pathak <officialabhishekpathak@gmail.com>
2024-07-07 06:51:15 +0530

    ci: fix workflow for backend

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,11 @@ require("dotenv").config()
 const { sendResponseMiddleware } = require("./src/middlewares/helpers")
 const router = require("./src/routes")
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 const app = express()
 
 app.use(cors({
-    // origin: process.env.FRONTEND_URL,
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
 }))
 app.use(express.json())
 app.use(sendResponseMiddleware)
